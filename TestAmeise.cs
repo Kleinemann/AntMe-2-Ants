dbad1de9a2a91067ce56025bb05d6c5b12bad5d9
Marcel Klein <33481835+Kleinemann@users.noreply.github.com>
2023-09-25 17:47:08 +0200

    Upload to github

---
+++ TestAmeise.cs
@@ -0,0 +1,55 @@
+﻿using AntMe_2_Lib.Definitions;
+using AntMe_2_Lib.GameObject;
+
+namespace AntMe_2_Ants
+{
+    [Player(ColonyName="Testameisen",
+        FirstName = "Marcel",
+        LastName = "Klein")]
+    [Caste(
+        Name = "Test",
+        AttackModifier = 0,
+        EnergyModifier = 0,
+        LoadModifier = 0,
+        RangeModifier = 0,
+        RotationSpeedModifier = 0,
+        SpeedModifier = 0,
+        ViewRangeModifier = 0
+    ),
+    Caste(
+        Name = "Schnell",
+        AttackModifier = -1,
+        EnergyModifier = -1,
+        LoadModifier = -1,
+        RangeModifier = -1,
+        RotationSpeedModifier = -1,
+        SpeedModifier = 6,
+        ViewRangeModifier = -1
+        )]
+    public class TestAmeise : AntCore
+    {
+        static string lastCaste = string.Empty;
+
+        public override string CasteSelect()
+        {
+            string caste = lastCaste == "Test" ? "Schnell" : "Test";
+            lastCaste = caste;
+            return caste;
+        }
+
+        public override void Init()
+        {
+            ShowDebugInfo(" erwacht zum Leben");
+        }
+
+        public override void Waiting()
+        {
+            ShowDebugInfo("wartet");
+        }
+
+        public override void Tick()
+        {
+            ShowDebugInfo("Tick");
+        }
+    }
+}
---
+++ TestAmeise2.cs
@@ -0,0 +1,51 @@
+﻿using AntMe_2_Lib.Definitions;
+using AntMe_2_Lib.GameObject;
+
+namespace AntMe_2_Ants
+{
+    [Player(ColonyName = "Hercules 2",
+        FirstName = "Marcel",
+        LastName = "Klein")]
+    [Caste(
+        Name = "Stark",
+        AttackModifier = 1,
+        EnergyModifier = 1,
+        LoadModifier = -2,
+        RangeModifier = 0,
+        RotationSpeedModifier = 0,
+        SpeedModifier = 0,
+        ViewRangeModifier = 0
+    ),
+    Caste(
+        Name = "Späher",
+        AttackModifier = -1,
+        EnergyModifier = -1,
+        LoadModifier = -1,
+        RangeModifier = -1,
+        RotationSpeedModifier = 0,
+        SpeedModifier = 2,
+        ViewRangeModifier = 2
+        )]
+    public class TestAmeise2 : AntCore
+    {
+        public override string CasteSelect()
+        {
+            return "Stark";
+        }
+
+        public override void Init()
+        {
+            ShowDebugInfo(" wurde geboren");
+        }
+
+        public override void Waiting()
+        {
+            
+        }
+
+        public override void Tick()
+        {
+            ShowDebugInfo(" tiktak");
+        }
+    }
+}
